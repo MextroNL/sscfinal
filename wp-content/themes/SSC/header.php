a724ca198363aca279817c276c423d59b6b32222
Hamish Caldow <Hcaldow@student.scalda.nl>
2018-02-27 13:57:29 +0100

    page title final

--- wp-content/themes/SSC/header.php
+++ wp-content/themes/SSC/header.php
@@ -57,7 +57,7 @@
         <div class="metaslider">
             <?php echo do_shortcode('[metaslider id="67"]'); ?>
             <div class="pagename">
-                <?php echo get_query_var('pagename'); ?>
+                <?php echo wp_title(''); ?>
             </div>
             <!-- Button "Project aanvragen" op de Home -->
 
