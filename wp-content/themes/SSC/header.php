f7acdc4aa16e821254407de6044f1dadf0c627b4
Mextro <mdenouden@student.scalda.nl>
2018-02-27 13:34:45 +0100

    Echo wp_title

--- wp-content/themes/SSC/header.php
+++ wp-content/themes/SSC/header.php
@@ -56,8 +56,10 @@
     <div class="uk-width-1-1 metasliderblok">
         <div class="metaslider">
             <?php echo do_shortcode('[metaslider id="67"]'); ?>
+            </div>
             <div class="pagename">
                 <?php echo get_query_var('pagename'); ?>
+                <?php echo wp_title(''); ?>
             </div>
             <!-- Button "Project aanvragen" op de Home -->
 
