56dd768887af8f7411f017138ad8b8f8478c81bf
Mextro <mdenouden@student.scalda.nl>
2018-03-05 08:48:42 +0100

    Hovers

--- wp-content/themes/SSC/projecten-page.php
+++ wp-content/themes/SSC/projecten-page.php
@@ -33,20 +33,17 @@
         <?php if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
 
             <div class="uk-width-large-1-3 uk-width-medium-1-2 uk-width-small-1-1 projectengrid">
-                <?php $featured = get_the_post_thumbnail_url($post->ID, 'full', true); ?>
+                <div class="project-box">
 
-                    <!-- Project Boxes-->
-<!--                <div class="project-box">-->
-                        <div class="projectfoto" style="background-image: url('<?php echo $featured ?>');">
-                            <a id="imglink" href="<?php the_permalink(); ?>"></a>
-<!--                            <div class="project-overlay">-->
-<!--                                <div class="project-excerpt">--><?php //the_excerpt(); ?><!--</div>-->
-<!--                            </div>-->
+                    <?php $featured = get_the_post_thumbnail_url($post->ID, 'full', true); ?>
+
+
+                    <a id="imglink" href="<?php the_permalink(); ?>"> <div class="projectfoto" style="background-image: url('<?php echo $featured ?>');">
+                        <div class="project-excerpt"><?php the_excerpt(); ?></div>
+                       </a>
                     </div>
-<!--                </div>-->
-
-                <a class="meerinformatie" target="_blank"
-                   href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a>
+                    <a class="meerinformatie" target="_blank" href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a>
+                </div>
             </div>
 
         <?php endwhile;
