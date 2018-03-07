1d8ecff816dd4676715f74284697896c4ba027d7
Mextro <mdenouden@student.scalda.nl>
2018-03-07 18:20:43 +0100

    Copyright, site title favicon

--- wp-content/themes/SSC/header.php
+++ wp-content/themes/SSC/header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <title>SSC</title>
+
+    <title>Shared Service Center</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="<?php echo home_url('wp-content/themes/SSC/js/uikit.min.js'); ?>"></script>
     <script src="https://use.fontawesome.com/46636d4f0f.js"></script>
 
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/uikit.min.css"/>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
