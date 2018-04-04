bd1fefd8d8101b89ef086af2e5983acd63e6396f
Mextro <mdenouden@student.scalda.nl>
2018-04-04 13:28:47 +0200

    Footer link color

--- wp-content/themes/SSC/footer.php
+++ wp-content/themes/SSC/footer.php
@@ -2,14 +2,14 @@
     <div class="uk-width-1-1 footercontent">
         <table>
             <tr>
-                <th><i class="fa fa-map-marker" aria-hidden="true"></i><a href="https://www.google.com/maps/place/Edisonweg+4a,+4382+NW+Vlissingen/@51.4516605,3.5866463,17z/data=!3m1!4b1!4m5!3m4!1s0x47c499f84d870ded:0xee92851f1d8da16d!8m2!3d51.4516572!4d3.588835">
+                <th><i class="fa fa-map-marker" aria-hidden="true"></i><a id="con-link" href="https://www.google.com/maps/place/Edisonweg+4a,+4382+NW+Vlissingen/@51.4516605,3.5866463,17z/data=!3m1!4b1!4m5!3m4!1s0x47c499f84d870ded:0xee92851f1d8da16d!8m2!3d51.4516572!4d3.588835">
                         Edisonweg 4a, Vlissingen</a></th>
             </tr>
             <tr>
-                <td><i class="fa fa-phone" aria-hidden="true"></i><a href="tel:0118 558 842"</a>0118 558 842</td>
+                <td><i class="fa fa-phone" aria-hidden="true"></i><a id="con-link" href="tel:0118 558 842"</a>0118 558 842</td>
             </tr>
             <tr>
-                <td><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:projecten@ssc-scalda.nl">projecten@ssc-scalda.nl</a></td>
+                <td><i class="fa fa-envelope" aria-hidden="true"></i><a id="con-link" href="mailto:projecten@ssc-scalda.nl">projecten@ssc-scalda.nl</a></td>
             </tr>
         </table>
 
